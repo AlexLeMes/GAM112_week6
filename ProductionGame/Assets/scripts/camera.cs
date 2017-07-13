7c954147faf481e23f4a7fcc7ff75af4544acc49
AlexLeMes <alexlem17@gmail.com>
2017-07-13 14:18:25 +0400

    WEEK7

---
+++ ProductionGame/Assets/scripts/camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+
+
+    public Transform playerT;
+
+    public float xOffSet = -15f;
+    public float yOffSet = 8f;
+    public float zOffSet = 0f;
+
+
+    void Update ()
+    {
+        transform.position = new Vector3(playerT.position.x + xOffSet, playerT.position.y + yOffSet, playerT.position.z + zOffSet);
+	}
+}
